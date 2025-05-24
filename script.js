51653be70f9481ca9ad1effdee8e23422329686b
ShubhamVishvakarma134 <90124726+ShubhamVishvakarma134@users.noreply.github.com>
2025-05-25 03:35:01 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const faqQuestions = document.querySelectorAll('.faq-question');
+
+    faqQuestions.forEach(question => {
+        question.addEventListener('click', () => {
+            const faqItem = question.closest('.faq-item');
+            faqItem.classList.toggle('active');
+        });
+    });
+});
+
+
